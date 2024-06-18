48ec7af2aacf6d146d37bec9b6ea8b7b7551feb2
Praaaaks <praksatyal@gmail.com>
2024-06-19 01:05:18 +0545

    Quicksort completed

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
 #include<random>
+#include<ctime>
+#include<chrono>
 
 int Partition(int* a, int low, int high){
-    std::cout<<"Partition Called";
     int pi = a[low];
     int i = low;
     int j = high + 1;
-    while( i < j ){
-        while (i<j && a[j]<pi) { j -= 1; }
-        while (i<j && a[i]>pi) { i += 1; }
-        if( i < j ){
-            int temp1 = a[j];
-            a[j] = a[i];
-            a[i] = temp1;
+    while (true) {
+        while (a[++i] < pi) {
+            if (i == high) break;
         }
+        
+        while (a[--j] > pi) {
+            if (j == low) break;
+        }
+        
+        if (i >= j) break;
+        
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
+
     //Swap low with j
     int temp2 = a[low];
     a[low] = a[j];
@@ -24,7 +32,6 @@ int Partition(int* a, int low, int high){
 }
 
 void QuickSort(int* a, int low, int high){
-    std::cout<<"QuickSort called\n";
     if(low < high){
         int pivot = Partition(a, low, high);
         QuickSort(a, low, pivot-1);
@@ -36,20 +43,38 @@ int main(){
     int n;
     std::cout<<"Enter size of list: ";
     std::cin>>n;
-    int arr[n];
+    int arr[n], arr2[n];
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 999);
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
+    // std::uniform_int_distribution<> dis(0, 999);
 
-    std::cout<<"Using MT:\n";
+    srand(time(NULL));
+
     for(int i = 0; i < n; i++)
-        arr[i] = dis(gen);
-    
-    std::cout<<"Randomization completed\n";
+        {arr[i] = rand();
+        // std::cout<< arr[i] <<", ";
+        }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
     QuickSort(arr, 0, n-1);
 
-    std::cout<<"Sorted List: \n";
-    for(int i = 0; i < n; i++)
-        std::cout<<arr[i]<<", ";
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout<<"Execution Time for average case is: "<< elapsed.count()<<std::endl;
+
+    for(int i = 0; i < n; i++){
+        arr2[i] = n - i;
+    }
+
+    auto start2 = std::chrono::high_resolution_clock::now();
+
+    QuickSort(arr2, 0, n-1);
+
+    auto finish2 = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed2 = finish2 - start2;
+    std::cout<<"Execution Time for worst case is: "<< elapsed2.count();    
 }
